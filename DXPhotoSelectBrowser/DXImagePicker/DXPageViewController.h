492344a79c8bd4e183ddc916b7393ac88496616c
jzhou <jzhou@ilegendsoft.com>
2014-12-12 11:49:58 +0800

    update

---
+++ DXPhotoSelectBrowser/DXImagePicker/DXPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  DXPageViewController.h
+//  DXPhotoSelectBrowser
+//
+//  Created by zhoujiuhai on 14/12/12.
+//  Copyright (c) 2014年 xiekw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DXPageViewController : UIViewController
+
+@end
---
+++ DXPhotoSelectBrowser/DXImagePicker/DXPageViewController.m
@@ -0,0 +1,37 @@
+//
+//  DXPageViewController.m
+//  DXPhotoSelectBrowser
+//
+//  Created by zhoujiuhai on 14/12/12.
+//  Copyright (c) 2014年 xiekw. All rights reserved.
+//
+
+#import "DXPageViewController.h"
+
+@interface DXPageViewController ()
+
+@end
+
+@implementation DXPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
